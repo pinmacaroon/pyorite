f2f275875120b5158f3b3a473e764748e82327b5
Pridalkó Márk <mark.pridalko@gmail.com>
2025-01-16 21:46:19 +0100

    start of item adding from bloomery and pyorite(old)

--- src/main/java/pin/macaroon/pyorite/Pyorite.java
+++ src/main/java/pin/macaroon/pyorite/Pyorite.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pin.macaroon.pyorite.items.ModItems;
 
 public class Pyorite implements ModInitializer {
 	public static final String MOD_ID = "pyorite";
@@ -13,7 +14,7 @@ public class Pyorite implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.regModItems();
 
 		LOGGER.info("I am alive and ready to take over your world :)");
 	}
---
+++ src/main/java/pin/macaroon/pyorite/items/ModItems.java
@@ -0,0 +1,9 @@
+package pin.macaroon.pyorite.items;
+
+import pin.macaroon.pyorite.Pyorite;
+
+public class ModItems {
+    public static void regModItems(){
+        Pyorite.LOGGER.info("registering items");
+    }
+}
