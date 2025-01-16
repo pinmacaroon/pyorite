a4fdace6943b4b0c814bb173d794793b34034e0e
Pridalkó Márk <mark.pridalko@gmail.com>
2025-01-16 21:13:20 +0100

    initial commit

---
+++ src/main/java/pin/macaroon/pyorite/PyoriteDataGenerator.java
@@ -0,0 +1,11 @@
+package pin.macaroon.pyorite;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PyoriteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
