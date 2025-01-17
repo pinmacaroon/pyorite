901e4f40dc0e6fe4fcae1202d64dd43928c876c3
Pridalkó Márk <mark.pridalko@gmail.com>
2025-01-17 17:56:07 +0100

    migrating pyorite(old) functionality, updated .gitignore

---
+++ src/main/java/pin/macaroon/pyorite/items/ModArmorMaterials.java
@@ -0,0 +1,81 @@
+package pin.macaroon.pyorite.items;
+
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import pin.macaroon.pyorite.Pyorite;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    PYORITE_CHUNK( "pyorite", 25, new int[]{ 3, 8, 6, 4},  19,
+            SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 2f, 0, () -> Ingredient.ofItems(ModItems.PYORITE_CHUNK));
+
+    ;
+
+
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float thoughness;
+    private final float knockbackResistance;
+    private final Supplier<Ingredient> repairIngredient;
+
+    private static int[] BASE_DURABILITY = { 11, 16, 15, 13 };
+
+    ModArmorMaterials(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, SoundEvent equipSound, float thoughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.thoughness = thoughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return BASE_DURABILITY[type.ordinal()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return protectionAmounts[type.ordinal()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @Override
+    public String getName() {
+        return Pyorite.MOD_ID + ":" + this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.thoughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
--- src/main/java/pin/macaroon/pyorite/items/ModItems.java
+++ src/main/java/pin/macaroon/pyorite/items/ModItems.java
@@ -2,8 +2,10 @@ package pin.macaroon.pyorite.items;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.SwordItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -27,6 +29,13 @@ public class ModItems {
     public static final Item CREEPER_COOKIE = registerItem("creeper_cookie", new Item(new FabricItemSettings().food(ModFoodComponents.CREEPER_COOKIE).maxCount(64)));
     public static final Item AEROITE = registerItem("aeroite", new Item(new FabricItemSettings().rarity(Rarity.RARE)));
     public static final Item HANDGUN = registerItem("handgun", new GunItem(new FabricItemSettings().maxDamage(60), 10, ModItemTags.AMMO, 6D));
+    public static final Item GILDED_NETHERITE_HOLY_GREATSWORD = registerItem( "gilded_netherite_holy_greatsword", new SwordItem(ModToolMaterial.HOLY, 10, -3.5f, new FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON)));
+    public static final Item SHWRAPPEL = registerItem("shwrappel",new Item(new FabricItemSettings().rarity(Rarity.COMMON)));
+    public static final Item PYORITE_CHUNK = registerItem("pyorite_chunk",new Item(new FabricItemSettings().rarity(Rarity.COMMON)));
+    public static final Item PYORITE_HELMET = registeritem( "pyorite_helmet", new ArmorItem(ModArmorMaterials.PYORITE_CHUNK, ArmorItem.Type.HELMET, new FabricItemSettings()));
+    public static final Item PYORITE_CHESTPLATE = registeritem( "pyorite_chestplate", new ArmorItem(ModArmorMaterials.PYORITE_CHUNK, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
+    public static final Item PYORITE_LEGGINGS = registeritem( "pyorite_leggings", new ArmorItem(ModArmorMaterials.PYORITE_CHUNK, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
+    public static final Item PYORITE_BOOTS = registeritem( "pyorite_boots", new ArmorItem(ModArmorMaterials.PYORITE_CHUNK, ArmorItem.Type.BOOTS, new FabricItemSettings()));
 
 
     public static void regModItems(){
@@ -52,5 +61,8 @@ public class ModItems {
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(Pyorite.MOD_ID, name), item);
+    }
+    private static Item registeritem(String name, Item item){
+        return registerItem(name, item);
     }
 }
