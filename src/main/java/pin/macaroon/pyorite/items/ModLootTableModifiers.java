6295538eee8ba2d3d2df394c3c534f9701dc87a1
Pridalkó Márk <mark.pridalko@gmail.com>
2025-01-20 15:47:54 +0100

    fixed and added loot table(s)

--- src/main/java/pin/macaroon/pyorite/items/ModLootTableModifiers.java
+++ src/main/java/pin/macaroon/pyorite/items/ModLootTableModifiers.java
@@ -1,5 +1,6 @@
 package pin.macaroon.pyorite.items;
 
+import net.fabricmc.fabric.api.loot.v2.FabricLootTableBuilder;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
@@ -12,6 +13,8 @@ import net.minecraft.util.Identifier;
 public class ModLootTableModifiers {
     private  static final Identifier ANCIENT_CITY_IP =
             new Identifier("minecraft", "chests/ancient_city");
+    private  static final Identifier CREEPER_DROP =
+            new Identifier("minecraft", "entities/creeper");
 
 
     public static void modifyLootTables() {
@@ -19,17 +22,18 @@ public class ModLootTableModifiers {
             if (ANCIENT_CITY_IP.equals(identifier)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.3f)) // Loot chances are 30% meaning this is how rare the item is to spawn in the set structure chest.
+                        .conditionally(RandomChanceLootCondition.builder(0.3f)) //ch in %
                         .with(ItemEntry.builder(ModItems.ELEMENTITE_TEMPLATE))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
-
-                tableBuilder.pool(poolBuilder.build());
-
-
-
+                builder.pool(poolBuilder.build());
+            } else if (CREEPER_DROP.equals(identifier)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.1f)) //ch in %
+                        .with(ItemEntry.builder(ModItems.SHWRAPPEL))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                builder.pool(poolBuilder.build());
             }
-
-
 
 
         });
