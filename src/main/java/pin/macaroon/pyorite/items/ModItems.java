5c971606b546ee3c006f9b96f31382e800f8ca47
boxed <boxedboi0@gmail.com>
2025-03-01 21:58:35 +0100

    Normalized the popsicles speed

--- src/main/java/pin/macaroon/pyorite/items/ModItems.java
+++ src/main/java/pin/macaroon/pyorite/items/ModItems.java
@@ -35,7 +35,7 @@ public class ModItems {
     public static final Item PYORITE_LEGGINGS = registeritem( "pyorite_leggings", new ArmorItem(ModArmorMaterials.PYORITE_CHUNK, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
     public static final Item PYORITE_BOOTS = registeritem( "pyorite_boots", new ArmorItem(ModArmorMaterials.PYORITE_CHUNK, ArmorItem.Type.BOOTS, new FabricItemSettings()));
     public static final Item PRETTY_PINK_HEART = registeritem( "pretty_pink_heart", new Item(new FabricItemSettings()));
-    public static final Item ICE_SWORD = registerItem("popsicle", new IceSwordItem(ModToolMaterial.ICE, 3, 3, new FabricItemSettings()));
+    public static final Item ICE_SWORD = registerItem("popsicle", new IceSwordItem(ModToolMaterial.ICE, 3, -3F, new FabricItemSettings()));
     public static final Item MAILCE = registerItem("mailce", new MailceItem(ToolMaterials.IRON, new FabricItemSettings().maxDamage(128)));
 
 
